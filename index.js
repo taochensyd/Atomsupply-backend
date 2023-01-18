dc9ffd90f42e4b6226464b4e862a8349d51eb8b1
Tao Chen <taochensyd@gmail.com>
2023-01-19 05:35:27 +0800

    add cors

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ const sendApprovalEmail = require('./routes/approvalEmail');
 
 
 app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, "build")));
 app.use(express.urlencoded({ extended: false }))
 
 app.get("/", (req, res) => {
