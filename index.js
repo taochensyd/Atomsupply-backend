25420d2034f15d8c8ba3dc194386c1bacd7deab4
Tao Chen <taochensyd@gmail.com>
2023-01-19 05:05:17 +0800

    change port number

--- index.js
+++ index.js
@@ -34,6 +34,6 @@ app.use('/api/v1/sendApprovalEmail', sendApprovalEmail);
 
 
 
-const port = process.env.PORT || 5850;
+const port = process.env.PORT || 3500;
 
 app.listen(port, console.log(`Server is listening on port ${port}`));
